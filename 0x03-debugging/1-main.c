b7ec554ea132713288c3ab3e1b60e87857940cab
kingdavid1111 <kingdavidautofx@gmail.com>
2022-10-19 08:58:49 -0700

    mkhggvmlkj

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,12 +1,14 @@
-#ifndef main_c
-#define main_c
-
 #include <stdio.h>
 
 /**
 * main - causes an infinite loop
 * Return: 0
 */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 int main(void)
 {
@@ -24,4 +26,4 @@ int main(void)
 
         return (0);
 }
-
+/* main_c */
