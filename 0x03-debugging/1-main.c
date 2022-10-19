9422b72222240f03eedd723c54f7b9ba48bf54d4
kingdavid1111 <kingdavidautofx@gmail.com>
2022-10-19 09:09:48 -0700

    uehgwgsauh

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,7 @@
 int main(void)
 {
         int i;
-
         printf("Infinite loop incoming :(\n");
-
         i = 0;
 	  /**
         *{
@@ -20,7 +18,6 @@ int main(void)
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
-
 }
 
 /* main_c */
