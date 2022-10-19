ae199fe4a094ffd56c2e8eed568f6f3ed61e5517
kingdavid1111 <kingdavidautofx@gmail.com>
2022-10-19 08:31:23 -0700

    jsksiisj

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,9 +12,9 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
-        {
-                putchar(i);
-        }
+        *{
+        */      putchar(i);
+        *}
 
         printf("Infinite loop avoided! \\o/\n");
 
