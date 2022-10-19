77f8e9286cc421a432fae7fc898a64f22f834abe
kingdavid1111 <kingdavidautofx@gmail.com>
2022-10-19 09:07:16 -0700

    lsaoidsjk

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,11 +5,6 @@
 * Return: 0
 */
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-
 int main(void)
 {
         int i;
@@ -25,5 +20,7 @@ int main(void)
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
+
 }
+
 /* main_c */
